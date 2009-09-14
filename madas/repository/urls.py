871bae4680ba56b881f9741ae69605bf6339412a
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2009-09-14 04:59:57 +0000

    ssl

--- madas/repository/urls.py
+++ madas/repository/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 urlpatterns = patterns(
     '',
 
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT, 'SSL':True}),
     
     (r'^login/processLogin', 'madas.repository.views.login', {'SSL':True}),
     (r'^login/processLogout', 'madas.repository.views.processLogout', {'SSL':True}),
