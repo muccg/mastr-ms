61a0a0f3836ec55af510e1e3e6ecbaacd1fcda40
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2010-01-13 07:33:15 +0000

    relocation of shared ext-3.1.0 and rejigging code to work

--- madas/static/repo/js/controller.js
+++ madas/static/repo/js/controller.js
@@ -373,7 +373,7 @@ Ext.madasLoadExperiment = function(expId) {
  */
 Ext.madasInitApplication = function() {
     //various global settings for Ext
-    Ext.BLANK_IMAGE_URL = 'static/repo/ext-3.1.0/resources/images/default/s.gif';
+    Ext.BLANK_IMAGE_URL = 'static/ext-3.1.0/resources/images/default/s.gif';
     Ext.QuickTips.init();
     
     // turn on validation errors beside the field globally
