144601d00bbf1140b2ef19c7c2365a867c1d7486
Andrew Macgregor <amacgregor@ccg.murdoch.edu.au>
2010-05-31 02:25:52 +0000

    adding named url to wsurls

--- madas/repository/wsurls.py
+++ madas/repository/wsurls.py
@@ -24,5 +24,5 @@ urlpatterns = patterns('madas.repository.wsviews',
     (r'^uploadFile[/]*$', 'uploadFile', {'SSL':True}),
     (r'^downloadFile[/]*$', 'downloadFile', {'SSL':True}),
     (r'^updateSingleSource/(?P<exp_id>\w+)[/]*$', 'update_single_source', {'SSL':True}),
-    (r'^generate_worklist/(?P<run_id>\w+)[/]*$', 'generate_worklist', {'SSL':True}),
+    url(r'^generate_worklist/(?P<run_id>\w+)[/]*$', 'generate_worklist', {'SSL':True}, name='generate_worklist')
 )
