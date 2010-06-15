207b5e0864ca086cc636d933e49a878a7d7a94d1
Andrew Macgregor <amacgregor@ccg.murdoch.edu.au>
2010-06-15 06:51:14 +0000

    tidying

--- madas/urls.py
+++ madas/urls.py
@@ -1,31 +1,30 @@
 from django.conf.urls.defaults import * 
 from django.conf import settings
 
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+from madas.repository import admin as repoadmin
+from madas.m import admin as madasadmin
+
 urlpatterns = patterns('',
-    # Example:
-    # (r'^madas/', include('madas.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
 #    (r'^(.*)/authorize', 'madas.madas.views.authorize'),
 #    (r'^(.*)/index', 'madas.madas.views.serveIndex'),
     (r'^status/', status_view),
     (r'^sync/', include('madas.mdatasync_server.urls')),
+
     # madasrepo
     (r'^repo/', include('madas.repository.urls')),
     (r'^ws/', include('madas.repository.wsurls')),
-    # madas
-    #(r'^repoadmin[/]*(.*)', admin.site.root),
+
+    # admin
     (r'^repoadmin/', include(admin.site.urls)),
+
+    # static
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT, 'SSL' : True}),
     (r'^javascript/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT, 'SSL' : True}),
+
+    # madas
     (r'^', include('madas.m.urls')),
 )
