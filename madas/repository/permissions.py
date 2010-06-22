6d57229934c301c14a7745bb5ef824b6f76bec81
Andrew Macgregor <amacgregor@ccg.murdoch.edu.au>
2010-06-22 06:27:39 +0000

    adding decorator to check group membership

--- madas/repository/permissions.py
+++ madas/repository/permissions.py
@@ -1,3 +1,6 @@
+from functools import wraps
+from django.utils.decorators import available_attrs
+from django.http import *
 
 PermissionsByRoles = {
     'noderep': (
@@ -17,3 +20,23 @@ class PermissionManager(object):
 
         return False
 
+
+
+def user_passes_test(test_func, response=None):
+    """
+    Decorator for views that checks that the user passes the given test,
+    and returns the given response if necessary, HttpResponseForbidden(403) by default. 
+    The test should be a callable that takes the user object and returns True if the user passes.
+    The response must be a HttpResponse or subclass.
+    Adapted from django.contrib.auth.decorators.user_passes_test
+    """
+    if not response or not isinstance(response, HttpResponse):
+        response = HttpResponseForbidden()
+
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if test_func(request.user):
+                return view_func(request, *args, **kwargs)
+            return response
+        return wraps(view_func, assigned=available_attrs(view_func))(_wrapped_view)
+    return decorator
