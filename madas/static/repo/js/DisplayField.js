0efd5f03cd253316ef7b52c660ced100e59e623a
Nick Takayama <ntakayama@ccg.murdoch.edu.au>
2010-07-26 07:06:39 +0000

    MADisplayField

---
+++ madas/static/repo/js/DisplayField.js
@@ -0,0 +1,99 @@
+/**
+ * a re-imagining of a DisplayField that submits its value rather than frustrating the developer
+ */
+Ext.form.MADisplayField = Ext.extend(Ext.form.Field,  {
+    validationEvent : false,
+    validateOnBlur : false,
+    defaultAutoCreate : {tag: "div"},
+    /**
+     * @cfg {String} fieldClass The default CSS class for the field (defaults to <tt>"x-form-display-field"</tt>)
+     */
+    fieldClass : "x-form-display-field",
+    /**
+     * @cfg {Boolean} htmlEncode <tt>false</tt> to skip HTML-encoding the text when rendering it (defaults to
+     * <tt>false</tt>). This might be useful if you want to include tags in the field's innerHTML rather than
+     * rendering them as string literals per the default logic.
+     */
+    htmlEncode: false,
+
+    // private
+    initEvents : Ext.emptyFn,
+
+    isValid : function(){
+        return true;
+    },
+
+    validate : function(){
+        return true;
+    },
+
+    getRawValue : function(){
+        var v = this.rendered ? this.el.dom.innerHTML : Ext.value(this.value, '');
+        if(v === this.emptyText){
+            v = '';
+        }
+        if(this.htmlEncode){
+            v = Ext.util.Format.htmlDecode(v);
+        }
+        return v;
+    },
+
+    getValue : function(){
+        return this.getRawValue();
+    },
+    
+    getName: function() {
+        return this.name;
+    },
+
+    setRawValue : function(v){
+        if(this.htmlEncode){
+            v = Ext.util.Format.htmlEncode(v);
+        }
+        if (this.rendered) {
+            if (this.inputEl) {
+                this.inputEl.value = v;
+            }
+        }
+        return this.rendered ? (this.el.dom.innerHTML = (Ext.isEmpty(v) ? '' : v)) : (this.value = v);
+    },
+
+    setValue : function(v){
+        this.setRawValue(v);
+        return this;
+    },
+    onRender : function(ct, position){
+            this.doc = Ext.isIE ? Ext.getBody() : Ext.getDoc();
+            Ext.form.MADisplayField.superclass.onRender.call(this, ct, position);
+    
+            this.wrap = this.el.wrap({cls: 'x-blahblahblahblah'});
+            this.inputEl = this.wrap.createChild({tag: "input", type:'hidden', name:this.getName(), value:''});
+            this.inputEl.value = this.getValue();
+    }
+    /** 
+     * @cfg {String} inputType 
+     * @hide
+     */
+    /** 
+     * @cfg {Boolean} disabled 
+     * @hide
+     */
+    /** 
+     * @cfg {Boolean} readOnly 
+     * @hide
+     */
+    /** 
+     * @cfg {Boolean} validateOnBlur 
+     * @hide
+     */
+    /** 
+     * @cfg {Number} validationDelay 
+     * @hide
+     */
+    /** 
+     * @cfg {String/Boolean} validationEvent 
+     * @hide
+     */
+});
+
+Ext.reg('madisplayfield', Ext.form.MADisplayField);
