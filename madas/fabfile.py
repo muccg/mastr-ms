87d98ad35c1a672774f99b66b865c76dfa946069
Brad Power <bpower@ccg.murdoch.edu.au>
2010-11-03 00:51:59 +0000

    Fabfile fixes for hudson

--- madas/fabfile.py
+++ madas/fabfile.py
@@ -12,39 +12,46 @@ env.svn_tags_url = "svn+ssh://store.localdomain/store/techsvn/ccg/madas/tags/"
 env.writeable_dirs.extend([]) # add directories you wish to have created and made writeable
 env.content_excludes.extend([]) # add quoted patterns here for extra rsync excludes
 env.content_includes.extend([]) # add quoted patterns here for extra rsync includes
+env.auto_confirm_purge = False #controls whether the confirmation prompt for purge is used
 
-def deploy():
+def deploy(auto_confirm_purge = False):
     """
     Make a user deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_deploy_user()
 
-def snapshot():
+def snapshot(auto_confirm_purge=False):
     """
     Make a snapshot deployment
     """
+    env.auto_confirm_purge=auto_confirm_purge
     _ccg_deploy_snapshot()
 
-def release():
+def release(auto_confirm_purge=False):
     """
     Make a release deployment
     """
+    env.auto_confirm=auto_confirm_purge
     _ccg_deploy_release()
 
-def testrelease():
+def testrelease(auto_confirm_purge=False):
     """
     Make a release deployment using the dev settings file
     """
+    env.auto_confirm=auto_confirm_purge
     _ccg_deploy_release(devrelease=True)
 
-def purge():
+def purge(auto_confirm_purge=False):
     """
     Remove the user deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_purge_user()
 
-def purge_snapshot():
+def purge_snapshot(auto_confirm_purge = False):
     """
     Remove the snapshot deployment
     """
+    env.auto_confirm_purge = auto_confirm_purge
     _ccg_purge_snapshot()
