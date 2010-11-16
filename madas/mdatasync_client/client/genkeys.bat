092e7cc8e94fbcb213a8d4de1b671726a8efaaec
Brad Power <bpower@ccg.murdoch.edu.au>
2010-11-16 01:27:53 +0000

    Installer fixes from last weeks testing with MA

--- madas/mdatasync_client/client/genkeys.bat
+++ madas/mdatasync_client/client/genkeys.bat
@@ -1,4 +1,6 @@
-ssh-keygen -q -b 1024 -t rsa -f id_rsa -N ''
+echo "CWRSYNC HOME IS: %CWRSYNCHOME%"
+echo "PATH is : %PATH%"
+ssh-keygen.exe -q -b 1024 -t rsa -f id_rsa -N ''
 mkdir "%CWRSYNCHOME%\home\%USERNAME%\.ssh\"
 move id_rsa "%CWRSYNCHOME%\home\%USERNAME%\.ssh\id_rsa"
 copy id_rsa.pub "%CWRSYNCHOME%\home\%USERNAME%\.ssh\"
