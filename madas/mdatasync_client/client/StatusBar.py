b850543df266f47f46059a1df9eb324bdad888f4
Brad Power <bpower@ccg.murdoch.edu.au>
2010-11-30 04:07:22 +0000

    Progress bar working

--- madas/mdatasync_client/client/StatusBar.py
+++ madas/mdatasync_client/client/StatusBar.py
@@ -1,18 +1,37 @@
 import wx
-from EnhancedStatusBar import EnhancedStatusBar
+import EnhancedStatusBar as ESB
 
-class StatusBar(EnhancedStatusBar):
+
+
+class StatusBar(ESB.EnhancedStatusBar):
     def __init__(self, parent, log):
-        EnhancedStatusBar.__init__(self, parent, -1)
+        ESB.EnhancedStatusBar.__init__(self, parent, -1)
+
+        self.STATUS_ICONS = {
+            'error' : wx.ArtProvider_GetBitmap(wx.ART_ERROR, wx.ART_TOOLBAR, (16,16)),
+            'warning' : wx.ArtProvider_GetBitmap(wx.ART_HELP, wx.ART_TOOLBAR, (16,16)),
+            'ok' :  wx.ArtProvider_GetBitmap(wx.ART_TICK_MARK, wx.ART_MESSAGE_BOX,(16,16))
+            }
+
 
         #Make the SB with 3 feilds
         self.SetFieldsCount(3)
+        self.SetSize((-1, 23))
         #Set the relative Widths
-        self.SetStatusWidths( [-1,-3,-1] )
+        self.SetStatusWidths( [-1,-3, -1] )
         self.log = log
         self.sizeChanged = False
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_IDLE, self.OnIdle)
+        self.progress =  wx.Gauge(self, -1)#, size=(400, 20))
+        #self.progresstext = wx.StaticText(self, -1, "Inactive")
+        self.statusicon = wx.StaticBitmap(self, -1, self.STATUS_ICONS['ok']) 
+        
+        self.AddWidget(self.progress, ESB.ESB_EXACT_FIT, ESB.ESB_EXACT_FIT, pos=1)
+        #self.AddWidget(self.progresstext, pos=1)
+        self.AddWidget(self.statusicon, pos=2)
+
+
+        #self.Bind(wx.EVT_SIZE, self.OnSize)
+        #self.Bind(wx.EVT_IDLE, self.OnIdle)
 
         #Field 0: Text
         #self.SetStatusText("status goes here", 0)
@@ -25,7 +44,8 @@ class StatusBar(EnhancedStatusBar):
         #self.Reposition()
 
         #Field 3: 
-        
+    
+    '''
     def OnSize(self, evt):
         self.Reposition() #for normal size events
         # Set a flag so the idle time handler will also do the repositioning.
@@ -37,11 +57,22 @@ class StatusBar(EnhancedStatusBar):
         if self.sizeChanged:
             self.Reposition()
 
-
     # reposition the checkbox
     def Reposition(self):
         rect = self.GetFieldRect(1)
-        #self.cb.SetPosition((rect.x+2, rect.y+2))
-        #self.cb.SetSize((rect.width-4, rect.height-4))
         self.sizeChanged = False
+    '''
 
+    def setTextStatus(self, text):
+        pass
+
+    def setProgress(self, value, progressdescription = ""):
+        self.progress.SetValue(value)
+        self.progresstext.SetValue(progressdescription)
+
+
+    def getProgress(self):
+        return self.progress.GetValue()
+
+    def setIconStatus(self, icon):
+        pass
