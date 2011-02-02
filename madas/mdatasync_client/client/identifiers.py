9e878cd022a4b15307b3346fc5027698520c5b45
Brad Power <bpower@ccg.murdoch.edu.au>
2011-02-02 03:42:12 +0000

    Fix for sync client log dir

--- madas/mdatasync_client/client/identifiers.py
+++ madas/mdatasync_client/client/identifiers.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 #This version must be incremented each build
-VERSION = "0.3.1"
+VERSION = "0.3.3"
 
 #We need a strange looking datadir because of esky.
 #Esky has an outer 'bootstrapper' exe, and all contents and
