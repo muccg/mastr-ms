f20b63a96d09cc76e89e45fb03474e1b243c93b5
bpower <bpower@ccg.murdoch.edu.au>
2011-11-04 17:52:44 +0800

    Changed settings.py to define base set of values, then override with appsettings and settings_local.py

--- madas/settings.py
+++ madas/settings.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# Django settings for project.
+"""
+Django settings import defaults for development server
+"""
 import os
 from django.utils.webhelpers import url
 
@@ -7,17 +9,14 @@ from django.utils.webhelpers import url
 if not os.environ.has_key('SCRIPT_NAME'):
     os.environ['SCRIPT_NAME']=''
 
-# PROJECT_DIRECTORY isnt set when not under wsgi
-if not os.environ.has_key('PROJECT_DIRECTORY'):
-    os.environ['PROJECT_DIRECTORY']=os.path.dirname(__file__).split("/appsettings/")[0]
-
 SCRIPT_NAME =   os.environ['SCRIPT_NAME']
 PROJECT_DIRECTORY = os.environ['PROJECT_DIRECTORY']
 
-
+#general site config
 DEBUG = True
 DEV_SERVER = True
 SITE_ID = 1
+APPEND_SLASH = True
 
 # https
 if SCRIPT_NAME:
@@ -29,26 +28,6 @@ else:
 TIME_ZONE = 'Australia/Perth'
 LANGUAGE_CODE = 'en-us'
 USE_I18N = True
-
-LOG_DIRECTORY = os.path.join(PROJECT_DIRECTORY, 'logs')
-TEMPLATE_DEBUG = DEBUG
-
-ROOT_URLCONF = 'madas.urls'
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'madas.mdatasync_server',
-    'madas.dashboard',
-    'madas.login',
-    'madas.quote',
-    'madas.users',
-    'madas.admin',
-    'madas.repository'
-]
 
 ##
 ## Django Core stuff
@@ -67,13 +46,14 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.ssl.SSLRedirect'
 ]
-TEMPLATE_DIRS = [
-    os.path.join(PROJECT_DIRECTORY,"templates","mako"), 
-    os.path.join(PROJECT_DIRECTORY,"templates"),
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
 ]
-
-
-MEMCACHE_KEYSPACE = "dev-madas-"
 
 AUTHENTICATION_BACKENDS = [
  'madas.repository.backend.MadasBackend',
@@ -81,33 +61,111 @@ AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.NoAuthModelBackend',
 ]
 
-SESSION_COOKIE_PATH = url('/')
-SESSION_SAVE_EVERY_REQUEST = True
-CSRF_COOKIE_NAME = "csrftoken_madas_repoadmin"
+#email
+EMAIL_HOST = 'ccg.murdoch.edu.au'
+SERVER_EMAIL = "apache@ccg.murdoch.edu.au"                      # from address
+RETURN_EMAIL = "apache@ccg.murdoch.edu.au"                      # from address
+EMAIL_SUBJECT_PREFIX = "DEV "
+RETURN_EMAIL = 'bpower@ccg.murdoch.edu.au'
+# default emails
+ADMINS = [
+    ( 'Tech Alerts', 'alerts@ccg.murdoch.edu.au' )
+]
+MANAGERS = ADMINS
 
+## Ldap
+AUTH_LDAP_SERVER = 'ldaps://fdsdev.localdomain'
+DEFAULT_GROUP = 'madas'  #this needs to exist in the database.
+AUTH_LDAP_BASE = 'ou=People,dc=ccg,dc=murdoch,dc=edu,dc=au'
+AUTH_LDAP_GROUP_BASE = 'ou=NEMA,ou=Web Groups,dc=ccg,dc=murdoch,dc=edu,dc=au'
+AUTH_LDAP_ADMIN_BASE = 'dc=ccg,dc=murdoch,dc=edu,dc=au'
+AUTH_LDAP_USER_BASE = 'ou=NEMA,ou=People,' + AUTH_LDAP_ADMIN_BASE
+LDAPADMINUSERNAME = 'uid=nemaapp,ou=Application Accounts'
+LDAPADMINPASSWORD = 'nr2WovGfkWR'
+AUTH_LDAP_GROUP = 'User'
+# dont require HTTPS for dev ldap
+LDAP_DONT_REQUIRE_CERT = True
+
+#Server side directories
 PERSISTENT_FILESTORE = os.path.normpath(os.path.join(PROJECT_DIRECTORY, '..', '..', 'files'))
-
 if "LOCALDEV" in os.environ:
     SSL_ENABLED = False
     PERSISTENT_FILESTORE = os.path.normpath('/tmp/madas/filedata')
-
-REPO_FILES_ROOT = PERSISTENT_FILESTORE
-QUOTE_FILES_ROOT = os.path.join(PERSISTENT_FILESTORE, 'quotes')
-MADAS_SESSION_TIMEOUT = 1800 #30 minute session timeout 
-
 #Ensure the persistent storage dir exits. If it doesn't, exit noisily.
 assert os.path.exists(PERSISTENT_FILESTORE), "This application cannot start: It expects a writeable directory at %s to use as a persistent filestore" % (PERSISTENT_FILESTORE) 
+# for local development, this is set to the static serving directory. For deployment use Apache Alias
+STATIC_SERVER_PATH = os.path.join(PROJECT_DIRECTORY,"static")
+# a directory that will be writable by the webserver, for storing various files...
+WRITABLE_DIRECTORY = os.path.join(PROJECT_DIRECTORY,"scratch")
+TEMPLATE_DIRS = [
+    os.path.join(PROJECT_DIRECTORY,"templates","mako"), 
+    os.path.join(PROJECT_DIRECTORY,"templates"),
+]
+# mako compiled templates directory
+MAKO_MODULE_DIR = os.path.join(WRITABLE_DIRECTORY, "templates")
+# mako module name
+MAKO_MODULENAME_CALLABLE = ''
+
+MEDIA_ROOT = os.path.join(PROJECT_DIRECTORY,"static")
+MEDIA_URL = '/static/'
+ADMIN_MEDIA_PREFIX = url('/static/admin-media/')
+
+LOG_DIRECTORY = os.path.join(PROJECT_DIRECTORY, 'logs')
+TEMPLATE_DEBUG = DEBUG
+LOGIN_URL = url('/accounts/login/')
+LOGOUT_URL = url('/accounts/logout/')
+
+
+#session and cookies
+MADAS_SESSION_TIMEOUT = 1800 #30 minute session timeout 
+SESSION_COOKIE_PATH = url('/')
+SESSION_SAVE_EVERY_REQUEST = True
+CSRF_COOKIE_NAME = "csrftoken_madas_repoadmin"
+SESSION_COOKIE_AGE = 60*60
 
 #functions to evaluate for status checking
 from status_checks import *
 STATUS_CHECKS = [check_default]
 
-APPEND_SLASH = True
-SITE_NAME = 'madas'
+# memcache server list
+MEMCACHE_SERVERS = ['memcache1.localdomain:11211','memcache2.localdomain:11211']
+MEMCACHE_KEYSPACE = "mastrms-dev"
 
-# these are non-standard and override defaults
-MEDIA_ROOT = os.path.join(PROJECT_DIRECTORY,"static")
-MEDIA_URL = '/static/'
+
+#APPLICATION SPECIFIC SETTINGS
+ROOT_URLCONF = 'madas.urls'
+SITE_NAME = 'madas'
+SECRET_KEY = 'qj#tl@9@7((%^)$i#iyw0gcfzf&#a*pobgb8yr#1%65+*6!@g$'
+EMAIL_APP_NAME = "MastrMS "
+REPO_FILES_ROOT = PERSISTENT_FILESTORE
+QUOTE_FILES_ROOT = os.path.join(PERSISTENT_FILESTORE, 'quotes')
+
+INSTALLED_APPS.extend([
+    'madas.mdatasync_server',
+    'madas.dashboard',
+    'madas.login',
+    'madas.quote',
+    'madas.users',
+    'madas.admin',
+    'madas.repository'])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<your database name here>',
+        'USER': '<your database user here>',
+        'PASSWORD': '<your database user password here>',
+        'HOST': '<your database host here>',                      
+        'PORT': '',                      
+    }
+}
+
+CHMOD_USER = 'apache'
+CHMOD_GROUP = 'maupload'
+
+#Datasync email settings:
+LOGS_TO_EMAIL = "<email address to receive datasync client log notifications>"
+KEYS_TO_EMAIL = "<email address to receive datasync key upload notifications>"
 
 
 ##
@@ -121,7 +179,7 @@ LOGS = ['mdatasync_server_log', 'madas_log']
 import ccglogging
 
 # Override defaults with your local instance settings.
-# They will be loaded from appsettings.ivecallocation, which can exist anywhere
+# They will be loaded from appsettings.<projectname>, which can exist anywhere
 # in the instance's pythonpath. This is a CCG convention designed to support
 # global shared settings among multiple Django projects.
 try:
@@ -129,9 +187,9 @@ try:
 except ImportError, e:
     pass
 
-#try an import of some local dev settings
+#If you have a local settings file which overrides these, try importing it here
 try:
-    from flat_settings_localdb import *
+    from settings_local import *
 except ImportError, e:
     pass
 
