ff4b6fbc2e0d57460d10f18fac6e46654f25ba5d
bpower <bpower@ccg.murdoch.edu.au>
2011-12-15 09:25:01 +0800

    import ldap data functionality

--- madas/repository/backend.py
+++ madas/repository/backend.py
@@ -2,7 +2,7 @@ from madas.users.user_manager import get_user_manager
 from django.contrib.auth.backends import ModelBackend
 from ccg.auth.backends import LDAPBackend
 from madas.repository.models import MadasUser
-
+print 'Madas Backend loaded'
 class MadasBackend(ModelBackend):
     '''
     Returns a MadasUser that has group info etc instead of the plain Django User.
@@ -16,11 +16,14 @@ class MadasBackend(ModelBackend):
         return user_manager.get_user_groups(user.username)
 
     def get_user(self, user_id):
+        print 'repository backend is getting user'
         try:
+            print 'repository backend is getting user'
             user = MadasUser.objects.get(pk=user_id)
             groups = self.get_groups(user)
             user.set_magroups(groups)
             return user
         except MadasUser.DoesNotExist:
+            print 'repository backend could not find user'
             return None
 
