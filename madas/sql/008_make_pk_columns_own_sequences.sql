77fdbf1eea2053ef316025cc16de1700aa6f4c2f
Tamas Szabo <szabtam@gmail.com>
2011-12-19 10:50:13 +0800

    Sequence of emailmap.id owns sequence emailmap_id_seq

--- madas/sql/008_make_pk_columns_own_sequences.sql
+++ madas/sql/008_make_pk_columns_own_sequences.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+ALTER SEQUENCE emailmap_id_seq OWNED BY emailmap.id;
 ALTER SEQUENCE formalquote_id_seq OWNED BY formalquote.id;
 ALTER SEQUENCE quotehistory_id_seq OWNED BY quotehistory.id;
 ALTER SEQUENCE quoterequest_id_seq OWNED BY quoterequest.id;
