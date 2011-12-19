a93af6bc0d58c6f9c43ffd2c8e9f1a1de7d629c0
Tamas Szabo <szabtam@gmail.com>
2011-12-19 09:30:47 +0800

    SQL script that makes all primary keys own their sequences, so Django 1.3 can still find the last inserted id. More details here: http://danostrowski.blogspot.com/2011/08/model-id-not-showing-up-after-save-with.html#!/2011/08/model-id-not-showing-up-after-save-with.html

---
+++ madas/sql/008_make_pk_columns_own_sequences.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+ALTER SEQUENCE formalquote_id_seq OWNED BY formalquote.id;
+ALTER SEQUENCE quotehistory_id_seq OWNED BY quotehistory.id;
+ALTER SEQUENCE quoterequest_id_seq OWNED BY quoterequest.id;
+
+COMMIT;
