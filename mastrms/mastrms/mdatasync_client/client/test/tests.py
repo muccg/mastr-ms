4e7e06033d63e2759c6820cc1f03720066e25101
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-06-21 11:05:45 +0800

    client tests: add some test cases for DataSyncServer
    Not all REST calls are unit tested yet, because I'm too lazy.

--- mastrms/mastrms/mdatasync_client/client/test/tests.py
+++ mastrms/mastrms/mdatasync_client/client/test/tests.py
@@ -1,9 +1,13 @@
 import unittest
+import dingus
+from StringIO import StringIO
+import logging
+logger = logging.getLogger(__name__)
+
 from mastrms.mdatasync_client.client.MSDataSyncAPI import DataSyncServer
 from mastrms.mdatasync_client.client.config import MSDSConfig
-import dingus
-
-from mastrms.mdatasync_client.client.test.testclient import *
+from mastrms.mdatasync_client.client.test.testclient import TestClient
+from mastrms.mdatasync_client.client.version import VERSION
 
 class BasicClientTests(unittest.TestCase):
     def setUp(self):
@@ -40,17 +44,83 @@ class BasicClientTests(unittest.TestCase):
         advanced.advanced_click_close()
         prefs.close()
 
+class _BetterPatcher(dingus._Patcher):
+    def __enter__(self):
+        self.patch_object()
+        return self.new_object
+def betterpatch(object_path, new_object=dingus.NoArgument):
+    module_name, attribute_name = object_path.rsplit('.', 1)
+    return _BetterPatcher(module_name, attribute_name, new_object)
+
 class DataSyncServerTests(unittest.TestCase):
     def setUp(self):
-        config = MSDSConfig(user="user",
-                            sitename=self.nc.site_name,
-                            stationname=self.nc.station_name,
-                            organisation=self.nc.organisation_name,
-                            localdir=unicode(self.simulator.destdir),
-                            synchub="%s/sync/" % self.live_server_url,
-                            logfile=logfile.name,
-                            loglevel=plogging.LoggingLevels.DEBUG,
-                            archivesynced=False,
-                            archivedfilesdir=archivedir)
+        self.config = MSDSConfig()
+        self.server = DataSyncServer(self.config)
 
-        self.server = DataSyncServer(config)
+    @staticmethod
+    def fake_urlopen(json, my_dingus=None):
+        my_dingus = my_dingus or dingus.Dingus()
+        urlopen = dingus.Dingus(return_value=StringIO(json))
+        return betterpatch("urllib2.urlopen", urlopen)
+
+    def test_handshake(self):
+        with self.fake_urlopen('{ "success": true, "details": { "host": "testhost", "flags": "testflags", "username": "username", "rootdir": "rootdir", "rules": ["a", "b", "c"] } }'):
+            details = self.server.handshake()
+
+            self.assertEqual(details["host"], "testhost")
+            self.assertListEqual(details["rules"], ["a", "b", "c"])
+
+    def test_handshake_weird_json(self):
+        with self.assertRaises(KeyError):
+            with self.fake_urlopen("{ lah: 'hello' }"):
+                details = self.server.handshake()
+
+    def test_handshake_not_json(self):
+        with self.assertRaises(KeyError):
+            with self.fake_urlopen("  *8>--O===3  this is not json; it's an emu  "):
+                details = self.server.handshake()
+
+    def test_requestsync(self):
+        with self.fake_urlopen('{ "success": true, "details": { "host": "testhost", "flags": "testflags", "username": "username", "rootdir": "rootdir", "rules": ["a", "b", "c"] } }') as d:
+            result = self.server.requestsync()
+            self.assertTrue(result["success"])
+            self.assertEqual(len(d.calls), 1)
+            self.assertEqual(len(d.calls("()")), 1)
+            self.assertTrue(d.calls("()", dingus.DontCare,
+                                    "sync_completed=False&version=%s" % VERSION,
+                                    dingus.DontCare).once())
+
+    def test_requestsync_expected_files(self):
+        # fixme: this is the wrong place to test such things
+        with self.fake_urlopen('{ "success": true, "details": {}, "files": { "1": { "test.txt": [1, 2, "relpath", false] } } }') as d:
+            result = self.server.requestsync()
+            logger.info(repr(d.calls))
+            self.assertTrue(result["success"])
+
+    def test_requestsync_baseurl(self):
+        "tests requestsync when configured sync url has no trailing slash"
+        self.config["synchub"] = "http://test"
+        self.config["sitename"] = "b"
+        self.config["stationname"] = "c"
+        self.config["organisation"] = "a"
+        my_dingus = dingus.Dingus()
+        with self.fake_urlopen('{ "success": true, "details": {} }') as d:
+            with dingus.patch("urllib2.Request", my_dingus):
+                result = self.server.requestsync()
+                self.assertTrue(result["success"])
+                self.assertTrue(d.calls("()").once())
+            self.assertEqual(len(my_dingus.calls), 1)
+            self.assertEqual(my_dingus.calls[0].args[0], "http://test/requestsync/a/b/c/")
+
+    def test_requestsync_urlopen_error(self):
+        "test requestsync when urlopen raises an exception"
+        with dingus.patch("urllib2.urlopen", dingus.exception_raiser(IOError("you fail"))):
+            result = self.server.requestsync()
+            self.assertFalse(result["success"])
+            self.assertEqual(result["message"], "you fail")
+
+    # todo: make tests for
+    #  * DataSyncServer.checksamplefiles()
+    #  * DataSyncServer.get_node_names()
+    #  * DataSyncServer.send_key()  - this uses yaphc
+    #  * DataSyncServer.send_log()  - this uses yaphc
