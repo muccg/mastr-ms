f4d158160492244817206754ffb3290bdeafa982
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-06-21 11:05:52 +0800

    tests: Put some common stuff in mastrms.testutils

--- mastrms/mastrms/testutils.py
+++ mastrms/mastrms/testutils.py
@@ -1,5 +1,8 @@
 from nose.plugins import Plugin
 import logging
+import os
+
+__all__ = ["SilenceSouthPlugin", "MockLoggingHandler", "XDisplayTest"]
 
 class SilenceSouthPlugin(Plugin):
     """
@@ -12,3 +15,52 @@ class SilenceSouthPlugin(Plugin):
     def configure(self, options, conf):
         super(SilenceSouthPlugin, self).configure(options, conf)
         logging.getLogger('south').setLevel(self.south_logging_level)
+
+class MockLoggingHandler(logging.Handler):
+    """
+    Mock logging handler to check for expected logs.
+    http://stackoverflow.com/a/1049375
+    """
+    def __init__(self, *args, **kwargs):
+        self.reset()
+        logging.Handler.__init__(self, *args, **kwargs)
+
+    def emit(self, record):
+        self.messages[record.levelname.lower()].append(record.getMessage())
+
+    def reset(self):
+        self.messages = {
+            'debug': [],
+            'info': [],
+            'warning': [],
+            'error': [],
+            'critical': [],
+        }
+
+class XDisplayTest(object):
+    """
+    This TestCase mixin provides a virtual X server if DISPLAY is not
+    set.
+    """
+    @classmethod
+    def setup_display(cls):
+        if not os.environ.get("DISPLAY"):
+            logging.info("Using Xvfb for display")
+            from xvfbwrapper import Xvfb
+            cls.vdisplay = Xvfb()
+            cls.vdisplay.start()
+        else:
+            cls.vdisplay = None
+
+    @classmethod
+    def teardown_display(cls):
+        if cls.vdisplay:
+            cls.vdisplay.stop()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.setup_display()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.teardown_display()
