80a34fb84286b120b3e1add1338bdfef2395d991
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-06-25 15:43:41 +0800

    tests: put nose plugins in separate module
    So nose isn't required to build docs.

--- mastrms/mastrms/testutils/__init__.py
+++ mastrms/mastrms/testutils/__init__.py
@@ -1,21 +1,7 @@
-from nose.plugins import Plugin
 import logging
 import os
 
-__all__ = ["SilenceSouthPlugin", "MockLoggingHandler", "XDisplayTest"]
-
-class SilenceSouthPlugin(Plugin):
-    """
-    South logs a lot while it does migrations in the test setup
-    stage. This nose plugin shuts it up, courtesy:
-    http://pypede.wordpress.com/2012/06/17/disable-south-debug-logging-when-testing-apps-with-nose-in-django/
-    """
-    south_logging_level = logging.ERROR
-
-    def configure(self, options, conf):
-        super(SilenceSouthPlugin, self).configure(options, conf)
-        logging.getLogger('south').setLevel(self.south_logging_level)
-        logging.getLogger("selenium.webdriver.remote.remote_connection").setLevel(self.south_logging_level)
+__all__ = ["MockLoggingHandler", "XDisplayTest"]
 
 class MockLoggingHandler(logging.Handler):
     """
