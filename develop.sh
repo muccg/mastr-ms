2439bbde79172a884268e62d498172585cc73957
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-07-02 16:05:01 +0800

    develop.sh: nose is required for testing

--- develop.sh
+++ develop.sh
@@ -17,7 +17,7 @@ AWS_BUILD_INSTANCE='aws_rpmbuild_centos6'
 AWS_STAGING_INSTANCE='aws_syd_mastrms_staging'
 TARGET_DIR="/usr/local/src/${PROJECT_NAME}"
 CLOSURE="/usr/local/closure/compiler.jar"
-TESTING_MODULES="argparse dingus xvfbwrapper"
+TESTING_MODULES="argparse dingus xvfbwrapper nose"
 MODULES="MySQL-python==1.2.3 psycopg2==2.4.6 Werkzeug flake8 ${TESTING_MODULES}"
 PIP_OPTS="-v -M --download-cache ~/.pip/cache"
 
