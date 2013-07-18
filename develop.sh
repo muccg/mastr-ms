98bf9e3a4597e81d096e5347f1feca0323ddfa70
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-07-18 17:38:39 +0800

    tests: bring back the nose plugins

--- develop.sh
+++ develop.sh
@@ -124,7 +124,7 @@ function ci_staging_tests() {
     sleep 2
 
     # Run tests, collect results
-    ccg ${AWS_STAGING_INSTANCE} dsudo:"cd ${REMOTE_TEST_DIR} && env DISPLAY\=\:0 dbus-launch ${PROJECT_NAME} test --noinput --with-xunit --xunit-file\=${REMOTE_TEST_RESULTS} ${TEST_LIST} ${EXCLUDES} || true"
+    ccg ${AWS_STAGING_INSTANCE} dsudo:"cd ${REMOTE_TEST_DIR} && env DISPLAY\=\:0 dbus-launch ${PROJECT_NAME} test --verbosity\=2 --noinput --with-xunit --xunit-file\=${REMOTE_TEST_RESULTS} ${TEST_LIST} ${EXCLUDES} || true"
     ccg ${AWS_STAGING_INSTANCE} getfile:${REMOTE_TEST_RESULTS},./
 }
 
