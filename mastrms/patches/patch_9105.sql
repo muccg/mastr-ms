ad1e59a03bd192530891b41ab3a646edea43d7f8
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-08-16 14:11:53 +0800

    anti-cruft: remove quaint old sql migration scripts

--- mastrms/patches/patch_9105.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-ALTER TABLE mdatasync_server_nodeclient ADD COLUMN "username" VARCHAR(255);
-ALTER TABLE mdatasync_server_nodeclient ADD COLUMN "hostname" VARCHAR(255);
-ALTER TABLE mdatasync_server_nodeclient ADD COLUMN "flags" VARCHAR(255);
-
-COMMIT;
--- mastrms/patches/patch_9326.sql
+++
@@ -1,4 +0,0 @@
-begin;
-alter table repository_sample add column sample_class_sequence smallint not null default 1;
-create index repository_sample_sample_class_sequence on repository_sample (sample_class_sequence);
-commit;
