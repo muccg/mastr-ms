78ea690fd6a20738c9fc10473c4092b12232982e
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2013-09-05 12:37:45 +0800

    MAS-36: Add some defaults and help text for NodeClient fields

--- mastrms/mastrms/mdatasync_server/models.py
+++ mastrms/mastrms/mdatasync_server/models.py
@@ -1,19 +1,28 @@
 from django.db import models
-from datetime import datetime, date, time
+from django.conf import settings
+from django.contrib.sites.models import Site
 from mastrms.mdatasync_server.rules import ActionType
 
-# Create your models here.
-
 class NodeClient(models.Model):
+    def default_default_data_path():
+        return getattr(settings, "REPO_FILES_ROOT", "")
+
+    def default_username():
+        return getattr(settings, "CHMOD_USER", "")
+
+    def default_hostname():
+        current_site = Site.objects.get(id=settings.SITE_ID)
+        return current_site.domain
+
     organisation_name = models.CharField(max_length=50)
     site_name = models.CharField(max_length=50)
     station_name = models.CharField(max_length=50)
-    default_data_path = models.CharField(max_length=255)
-    username = models.CharField(max_length=255, default="")
-    hostname = models.CharField(max_length=255, default="")
-    flags = models.CharField(max_length=255, default="")
-    date_created = models.DateTimeField(auto_now_add=True) #generate 'now' on INSERT
-    last_modified = models.DateTimeField(auto_now = True) #generate 'now' on UPDATE
+    default_data_path = models.CharField(max_length=255, default=default_default_data_path, help_text="File upload path on the Mastr-MS server")
+    username = models.CharField(max_length=255, default=default_username, help_text="Username to use for the rsync command")
+    hostname = models.CharField(max_length=255, default=default_hostname, help_text="rsync destination hostname")
+    flags = models.CharField(max_length=255, default="--protocol=30 -rzv --chmod=ug=rwX", help_text="Additional options for client to add to rsync command line")
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return "[%s]-[%s]-[%s]" % (self.organisation_name, self.site_name, self.station_name)
