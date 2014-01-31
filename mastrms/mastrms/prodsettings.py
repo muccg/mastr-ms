9731bea973b3df1faf44f950007d79f0d8ed0d23
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2014-01-31 14:10:32 +0800

    MAS-54 Add wider menu of settings in mastrms.conf and prodsettings.py

--- mastrms/mastrms/prodsettings.py
+++ mastrms/mastrms/prodsettings.py
@@ -4,7 +4,12 @@ from os import environ
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 def get_env_setting(setting, default=None):
-    """ Get the environment setting or return exception """
+    """
+    Get the environment setting, return a default value, or raise
+    an exception.
+    Values used by this function will likely come from
+    /etc/mastrms/mastrms.conf.
+    """
     try:
         return environ["_".join([PROJECT_NAME, setting])]
     except KeyError:
@@ -19,12 +24,52 @@ def get_env_setting(setting, default=None):
 
 DEBUG = False
 
-# extra email settings
-# see: https://docs.djangoproject.com/en/1.5/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = "PROD: "
+# Default SSL on and forced, turn off if necessary
+SSL_ENABLED = True
+SSL_FORCE = True
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
+# see: https://docs.djangoproject.com/en/1.5/ref/settings/#admins
+# see: https://docs.djangoproject.com/en/1.5/ref/settings/#managers
+ADMINS = [
+    ( 'alert', get_env_setting("alert_email", "root@localhost") )
+]
+MANAGERS = ADMINS
+
+RETURN_EMAIL = get_env_setting("return_email", "Mastr-MS <noreply@yoursite.com>")
+
+LOGS_TO_EMAIL = get_env_setting("logs_to_email", "log_email@yoursite.com")
+KEYS_TO_EMAIL = get_env_setting("keys_to_email", "key_email@yoursite.com")
+REGISTRATION_TO_EMAIL = get_env_setting("registration_to_email", "reg_email@yoursite.com")
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#server-email
+SERVER_EMAIL = get_env_setting("server_email", "apache@localhost")
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-host
+EMAIL_HOST = get_env_setting("email_host", "127.0.0.1")
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-host-password
+EMAIL_HOST_PASSWORD = get_env_setting("email_host_password", "")
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-host-user
+EMAIL_HOST_USER = get_env_setting("email_host_user", "")
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-port
+EMAIL_PORT = get_env_setting("email_port", 25)
+
+# see: https://docs.djangoproject.com/en/1.5/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = "PROD: "
+
+# See: https://docs.djangoproject.com/en/1.5/ref/settings/#email-use-tls
+EMAIL_USE_TLS = False
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 ALLOWED_HOSTS = get_env_setting("allowed_hosts", [])
 
 engines = {
@@ -43,6 +88,16 @@ DATABASES = {
         'PORT': get_env_setting("dbport", ""),
     }
 }
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+
+CHMOD_USER = get_env_setting("repo_user", "apache")
+CHMOD_GROUP = get_env_setting("repo_group", "apache")
+
+REPO_FILES_ROOT = get_env_setting("repo_files_root", os.path.join(CCG_WRITEABLE_DIRECTORY, 'files'))
+QUOTE_FILES_ROOT = get_env_setting("quote_files_root", os.path.join(CCG_WRITEABLE_DIRECTORY, 'quotes'))
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 SECRET_KEY = get_env_setting("secret_key", "")
 
